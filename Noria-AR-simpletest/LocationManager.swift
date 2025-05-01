bfc4489946f365e52e9fa3bfba05228dd2c0bf79
jesusarizaro <163051084+jesusarizaro@users.noreply.github.com>
2025-05-01 17:23:35 -0500

    Update LocationManager.swift

--- Noria-AR-simpletest/LocationManager.swift
+++ Noria-AR-simpletest/LocationManager.swift
@@ -7,13 +7,19 @@ class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
     override init() {
         super.init()
         manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
         manager.requestWhenInUseAuthorization()
         manager.startUpdatingLocation()
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.first {
+        if let location = locations.last {
             currentLocation = location
+            print("Ubicación obtenida: \(location.coordinate)")
         }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error al obtener ubicación: \(error)")
     }
 }
