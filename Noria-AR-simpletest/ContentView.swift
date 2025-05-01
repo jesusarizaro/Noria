27d646bdaa63fb001cc1a40513ca70ceb5cbbcb3
jesusarizaro <163051084+jesusarizaro@users.noreply.github.com>
2025-05-01 17:22:59 -0500

    Update ContentView.swift

--- Noria-AR-simpletest/ContentView.swift
+++ Noria-AR-simpletest/ContentView.swift
@@ -1,8 +1,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var shouldPlaceAnchors = false
+
     var body: some View {
-        ARViewContainer()
-            .edgesIgnoringSafeArea(.all)
+        VStack {
+            ARViewContainer(shouldPlaceAnchors: $shouldPlaceAnchors)
+                .edgesIgnoringSafeArea(.all)
+
+            Button(action: {
+                shouldPlaceAnchors.toggle()
+            }) {
+                Text("📍 Colocar Esferas")
+                    .font(.headline)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(12)
+            }
+            .padding()
+        }
     }
 }
